18029749d7aa6ae4470701862e6a40ec8100461e
Dain Im <dain98@gmail.com>
2022-12-05 07:37:41 -0500

    some testing

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
-// $(document).ready(function () {
-//     $('.chips').chips({
-//         onChipAdd: function(e) {
-//             console.log(e);
-//         },
-//         onChipDelete: () => {
-
-//         }
-//     })
-// })
+var sample = [
+    {
+        "team": "Red",
+        "players": [
+            "Dain",
+            "Seegii",
+            "Exarch",
+            "M I L E S"
+        ],
+        "score": "3,234,192"
+    },
+    {
+        "team": "Blue",
+        "players": [
+            "DigitalHypno",
+            "Cookiezi",
+            "Toy",
+            "Apraxia"
+        ],
+        "score": "4,293,232"
+    },
+    {
+        "team": "Green",
+        "players": [
+            "test1",
+            "test2",
+            "test3",
+            "test4"
+        ],
+        "score": "2,234,192"
+    },
+    {
+        "team": "Yellow",
+        "players": [
+            "test5",
+            "test6",
+            "test7",
+            "test8"
+        ],
+        "score": "8,234,128"
+    }
+]
+var button = document.getElementById('button');
+var mainBody = document.getElementById('main-body');
 
 var elems = document.querySelectorAll('.chips');
 var instances = M.Chips.init(elems,{
@@ -15,50 +49,89 @@ var instances = M.Chips.init(elems,{
     onChipDelete: () => onChipChange(instances)
 });
 
-function onChipChange(i) {
-    console.log(i[0].chipsData);
-}
-
-async function submit(){
-    button.classList.add("disabled");
-    var fields = document.getElementsByClassName("validate");
-    var matches = "";
-    var warmups = "";
-    for (var i = 0;i < fields.length;i++) {
-        if (i % 2) {
-            if (fields[i].value == "") {
-                warmups += "2,";
-            } else {
-                warmups += fields[i].value + ",";
-            }
-        } else {
-            var tempMatch = fields[i].value.split("/");
-            matches += tempMatch.pop() + ",";
-        }
-    }
-    var tbodyRef = document.getElementById('table').getElementsByTagName('tbody')[0];
-    const response = await fetch('https://match-costs-backend.herokuapp.com/matches/' + matches.replace(/,\s*$/, "") + '&' + warmups.replace(/,\s*$/, ""));
-    const costs = await response.json();
-    var new_tbody = document.createElement('tbody');
-    costs.forEach(function (player, index) {
-        var newRow = new_tbody.insertRow();
-        var newCell = newRow.insertCell();
-        var newText = document.createTextNode(player[0]); // PLAYER NAME
-        newCell.appendChild(newText);
-        var newCell = newRow.insertCell();
-        var newText = document.createTextNode(player[1]); // COST VALUE
-        newCell.appendChild(newText);
-    });
-    tbodyRef.parentNode.replaceChild(new_tbody,tbodyRef);
-    button.classList.remove("disabled");
-}
-
-function addField() {
+function editBody() {
     const div = document.createElement('div');
-    const form = document.getElementById('form');
-    console.log(form);
-    div.className = 'row';
-    div.innerHTML = `<div class="input-field col s9"><input id="mplink` + forms + `" type="text" class="validate white-text"><label for="mplink` + forms + `">MP Link #` + forms + `</label></div><div class="input-field col s3"><input id="warmup` + forms + `" type="text" class="validate white-text"><label for="warmup` + forms + `">Warmups</label></div>`
-    form.appendChild(div);
-    forms++;
+
+    div.className = "col s12";
+
+    div.innerHTML = 
+    `
+    <!-- Pagination -->
+    <ul class="pagination center">
+        <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+        <li class="active"><a href="#!">1</a></li>
+        <li class="waves-effect"><a href="#!">2</a></li>
+        <li class="waves-effect"><a href="#!">3</a></li>
+        <li class="waves-effect"><a href="#!">4</a></li>
+        <li class="waves-effect"><a href="#!">5</a></li>
+        <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+    </ul>
+    </div>`
+    
+    div.innerHTML = div.innerHTML + makeMapInfo().outerHTML + addTable().outerHTML;
+    mainBody.innerHTML = div.innerHTML;
 }
+
+function makeMapInfo() {
+    const mapData = {
+        "title": "xi - FREEDOM DIVE [FOUR DIMENSIONS] by Nakagawa-Kanon",
+        "metadata": "AR9, CS4, OD8, 7.58*"
+    }
+    const mapImgUrl = "https://assets.ppy.sh/beatmaps/39804/covers/cover.jpg";
+    const mapInfoDiv = document.createElement('div');
+
+    mapInfoDiv.innerHTML = mapInfoDiv.innerHTML + makeMapBg(mapImgUrl).outerHTML + makeMapData(mapData).outerHTML;
+    return mapInfoDiv;
+}
+
+function makeMapBg(mapImgUrl) {
+    const mapBgDiv = document.createElement('div');
+    mapBgDiv.className = "col s4";
+    mapBgDiv.innerHTML = `<img class="responsive-img" src="${mapImgUrl}">`;
+    return mapBgDiv;
+}
+
+function makeMapData(mapData) {
+    const mapInfoDiv = document.createElement('div');
+    mapInfoDiv.className = 'col s8';
+    mapInfoDiv.innerHTML = `
+    <h6>${mapData.title}</h6>
+    <h7>${mapData.metadata}</h7>`;
+    return mapInfoDiv;
+}
+
+function addTable() {
+    const table = document.createElement('table');
+    table.className = "highlight white-text";
+    table.innerHTML = `<thead>
+    <tr>
+        <th>Team</th>
+        <th>Players</th>
+        <th>Combined Score</th>
+    </tr>
+    </thead>
+    
+    <tbody>`
+    let tbody = ''
+    sample.forEach(row => {
+        tbody += `<tr>
+        <td>${row.team}</td>
+        <td>${row.players.join(", ")}</td>
+        <td>${row.score}</td>
+        </tr>`;
+    });
+    tbody += "</tbody>"
+    table.innerHTML += tbody;
+    return table;
+}
+
+function onChipChange(i) {
+    let a;
+    let chipsData = i[0].chipsData;
+    console.log(chipsData);
+    chipsData.forEach(chip => {
+        a += chip.tag + ", ";
+    })
+    console.log(a);
+}
+
